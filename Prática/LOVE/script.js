39b411a9a5e804e301e5eee85fba9a13c3800035
Alexandre D. Gomes <alexandredanielgomes524@gmail.com>
2025-02-19 14:25:56 +0000

    Initial commit

---
+++ Prática/LOVE/script.js
@@ -0,0 +1,4 @@
+document.getElementById('heartButton').addEventListener('click', function() {
+    const heartAnimation = document.getElementById('heartAnimation');
+    heartAnimation.classList.remove('hidden');
+});
