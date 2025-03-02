03d7fae918c8ada965efef65ba23fb4a6bee3baa
Alexandre D. Gomes <alexandredanielgomes524@gmail.com>
2025-03-02 14:38:13 +0000

    Atualizei projeto-clone-yu-gi-oh

--- Prática/Projeto-clone-yu-gi-io/SRC/js/index.js
+++ Prática/Projeto-clone-yu-gi-io/SRC/js/index.js
@@ -3,6 +3,19 @@ const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
+cartoes.forEach(cartao => {
+    cartao.addEventListener("click", function () {
+        const cartaVIrada = cartao.querySelector
+            (".carta-virada");
+
+        // virar o cartão
+        cartao.classList.toggle("virar");
+        // mostrar o fundo da carta
+        cartaVIrada.classList.toggle("mostrar-fundo-carta");
+        const descricao = cartao.querySelector(".descricao");
+        descricao.classList.toggle("esconder");
+    });
+});
 
 btnAvancar.addEventListener("click", function () {
     if (cartaoAtual === cartoes.length - 1) return;
