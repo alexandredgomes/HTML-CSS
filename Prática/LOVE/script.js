80df584f5d430e26f99a3f0306e68d63b8fc22ed
Alexandre D. Gomes <alexandredanielgomes524@gmail.com>
2025-04-11 17:13:32 +0100

    Eliminei alguns exercícios de prática

--- Prática/LOVE/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('heartButton').addEventListener('click', function() {
-    const heartAnimation = document.getElementById('heartAnimation');
-    heartAnimation.classList.remove('hidden');
-});
